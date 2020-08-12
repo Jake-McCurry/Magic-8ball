dd6b260b7d3c5c8dc8cbc2a9fe42dd1567f1d553
jake_m <jakewmccurry@gmail.com>
2020-08-12 15:48:09 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    var magic8Ball = {};
+    
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+magic8Ball.askQuestion = function(question){
+        var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+
+        $("#answer").text( answer );
+
+        console.log(question);
+        console.log(answer);
+    };
+	
+	$("#answer").hide();
+
+  var onClick = function() {
+        
+    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+    $("#answer").hide();
+      
+  var question = prompt("ASK A YES/NO QUESTION!");
+        magic8Ball.askQuestion(question);
+        $("#8ball").effect( "shake" );
+        $("#answer").fadeIn(500);
+   };
+
+  $("#questionButton").click( onClick );
+
+});
